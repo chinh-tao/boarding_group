01a058ee19f831d65a615b4ec71bf951b8e16354
ChinhTQ <chinhtq@saisystem.vn>
2022-10-27 16:20:36 +0700

    update project
    xoá logic xử lý lưu file ảnh

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:boarding_group/app/common/api.dart';
@@ -7,8 +8,6 @@ import 'package:boarding_group/app/modules/auth/auth_controller.dart';
 import 'package:boarding_group/app/modules/register/views/body/body_bottom_sheet.dart';
 import 'package:boarding_group/app/routes/app_pages.dart';
 import 'package:boarding_group/app/utils/utils.dart';
-import 'package:dio/dio.dart' as dio;
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -22,7 +21,6 @@ class RegisterController extends GetxController {
 
   final isLoadImage = false.obs;
   final isLoading = false.obs;
-  final isLoadUser = false.obs;
   final listError = ["", ""].obs;
 
   final _log = Logger();
@@ -120,60 +118,29 @@ class RegisterController extends GetxController {
     }
   }
 
-  Future<String> get getUrlImage async {
-    var urlImage = '';
-    try {
-      Reference ref = authController.storage
-          .refFromURL('gs://boarding-group.appspot.com')
-          .child(inputCode.text);
-      UploadTask task = ref.putFile(fileImage.value);
-      urlImage = await task.snapshot.ref.getDownloadURL();
-    } catch (err) {
-      _log.e('Fire Store: $err');
-      Utils.messWarning(MSG_SAVE_FILE);
-    }
-    return urlImage;
-  }
-
   Future<void> registerAccount() async {
     final form = <String, dynamic>{
       "id": inputCode.text,
       "email": inputEmail.text,
-      "images": await dio.MultipartFile.fromFile(fileImage.value.path,
-          filename: inputCode.text),
       "device_mobi": authController.device.value
     };
+    if (fileImage.value.path != '') {
+      final filedName = fileImage.value.path.split('/').last;
+      final typeFiled = filedName.split('.').last;
+      form["images"] = {
+        "file": base64Encode(fileImage.value.readAsBytesSync()),
+        "type": typeFiled
+      };
+    }
     isLoading.value = true;
-    final res = await api.post('/register',
-        data: form,
-        options: dio.Options(contentType: 'application/x-www-form-urlencoded'));
+    final res = await api.post('/register', data: form);
     isLoading.value = false;
     if (res.statusCode == 200 && res.data['code'] == 0) {
-      // if (fileImage.value.path != '') {
-      //   handeSaveImage();
-      // } else {
       Get.offNamed(Routes.LOGIN, parameters: {'category': '0'});
-      // }
     } else {
       Utils.messError(res.data['message']);
     }
     update();
-  }
-
-  void handeSaveImage() async {
-    var url = await getUrlImage;
-    print(url);
-    if (url != '') {
-      final res = await api.post('/register', data: {"images": url});
-      if (res.statusCode == 200 && res.data['code'] == 0) {
-        print('success');
-      }
-      //Get.offNamed(Routes.LOGIN, parameters: {'category': '0'});
-    } else {
-      while (url != '') {
-        handeSaveImage();
-      }
-    }
   }
 
   Future<void> handleCheckUser(String value) async {
