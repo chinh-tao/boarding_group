c4d30dba6c2fb786c26953610b7a7c09648a5385
luanlq <luanlq@saisystem.vn>
2022-11-25 08:50:09 +0700

    remove pass

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -29,7 +29,6 @@ class LoginController extends ChangeNotifier {
     if (arguments['category'] == '1') {
       userModel = arguments['user'];
       inputEmail.text = userModel.email!;
-      inputPass.text = 'luanlq@123';
     }
     notifyListeners();
   }
